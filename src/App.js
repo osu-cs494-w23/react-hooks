e35c29066a2f20974830417a7dfc008f232fc22b
Rob Hess <hessro@oregonstate.edu>
2023-01-25 15:30:19 -0800

    Add code from 1/25 lecture.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Clock(props) {
+    const [date, setDate] = useState(new Date())
+    useEffect(() => {
+        const timerId = setInterval(() => setDate(new Date()), 1000)
+        return () => clearInterval(timerId)
+    }, [])
+
     return (
-        <h2>It is {props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {date.toLocaleTimeString()}.</h2>
     );
 }
 
 function CountingButton() {
-    const count = 0;
+    // const count = 0;
+    const [count, setCount] = useState(0)
+    // const [pets, setPets] = useState({ cats: 2, fish: 3 })
+    // setPets({ fish: 2 })
+    const [cats, setCats] = useState(2)
+    const [fish, setFish] = useState(3)
+    const [dogs, setDogs] = useState(3)
+
+    useEffect(() => {
+        document.title = `${count} clicks`
+    }, [ count ])
+
     return (
         <div>
             <p>You clicked {count} times</p>
-            <button onClick={() => alert("The button was clicked.")}>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>
                 Click me
             </button>
         </div>
@@ -22,8 +39,9 @@ function App() {
     return (
         <div>
             <CountingButton />
+            <Clock />
         </div>
     );
 }
 
-export default App;
+export default App
